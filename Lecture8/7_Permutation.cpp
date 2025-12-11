1d28de76e6a56b7e2cb4c25275db43003c687046
Ajju912 <ajay61785@gmail.com>
2025-12-11 16:10:47 +0530

    update

--- Lecture8/7_Permutation.cpp
+++ Lecture8/7_Permutation.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-    
+    char a[]="aabbcc";
+    char b[]="bcaabc";
+    int freq[26]={0};
+    for(int i=0;a[i]!='\0';i++){
+        int ind=a[i]-'a';
+        freq[ind]++;
+    }
+    for(int i=0;b[i]!='\0';i++){
+        int ind=b[i]-'a';
+        freq[ind]--;
+    }
+    for(int i=0;i<26;i++){
+        if(freq[i]!=0){
+            cout<<"NOT PERMUTATION";
+            return 0;
+        }
+    }
+    cout<<"PERMUTATION";
+
     return 0;
 }
